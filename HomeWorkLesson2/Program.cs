75f4d9a862197d95a108c8daa417e3432d569134
Yuriy Slobodyanik <uslobodyanik@gmail.com>
2021-11-07 17:10:53 +0200

    Add project files.

---
+++ HomeWorkLesson2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HomeWorkLesson2
+{
+    class Program
+    {
+        public static int MathTwoNumber(int a, int b)
+        {
+            int result;
+
+            if (a > b)
+            {
+                result = a + b;
+            }
+            else if (a == b)
+            {
+                result = a * b;
+            }
+            else
+            {
+                result = a - b;
+            }
+
+            return result;
+        }
+
+        public static int InputWhithConsole()
+        {
+            Console.WriteLine("How many entering number? Enter number");
+            int enterNumberCount = Convert.ToInt32(Console.ReadLine());
+            return enterNumberCount;
+        }
+
+        static void Main(string[] args)
+        {
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(InputWhithConsole());
+        }
+    }
+}
